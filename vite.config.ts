37a2723843b57f3af16c72927772c16085256a16
Vuyo November <vuyonov@gmail.com>
2025-06-13 09:01:33 +0200

    Update Vite and Vercel configuration for production

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import postcssImport from 'postcss-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
